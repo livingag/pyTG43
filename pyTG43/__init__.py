54dc852ffdaba678956865ff93e38164b25a1cb0
Alex Livingstone <livingstone.alex@gmail.com>
2018-03-22 11:42:34 +1000

    restructure to improve calculation speed

--- pyTG43/__init__.py
+++ pyTG43/__init__.py
@@ -20,12 +20,15 @@ def tpsComp(rp, rs, directory):
 
     points = []
 
+    source = Source(rp, directory)
+    plan = Plan(source, rp, rs)
+
     for p in rp[0x300a, 0x10]:
         if Tag(0x300a,0x18) in p.keys():
             x, z, y = p[0x300a, 0x18].value
             name = p[0x300a, 0x16].value
             ref = p[0x300a, 0x12].value
-            points.append(DosePoint([x/10, y/10, z/10], rp, rs, name, ref, directory))
+            points.append(DosePoint([x/10, y/10, z/10], source, plan, name, ref, directory))
 
     table_data = [['Name','X','Y','Z','TPS (cGy)','Calc (cGy)','Diff (%)']]
 
@@ -40,6 +43,86 @@ def tpsComp(rp, rs, directory):
 
     print(SingleTable(table_data).table)
     return points
+
+
+class Source(object):
+    """Source parameters object.
+
+    Attributes:
+        L: length (cm).
+        Delta: dose rate constant (cGy h-1 U-1).
+        Sk: RAKR.
+        Fi(r,theta): anisotropy user data interpolation function.
+        gi(r): radial dose user data interpolation function.
+        G0: reference geometry function.
+    """
+    def __init__(self, rp, directory):
+        """
+        Args:
+            rp: pydicom object of RP file.
+            directory: directory containing source data.
+        """
+        r0 = 1
+        theta0 = np.pi/2
+        self.Sk = rp.SourceSequence[0].ReferenceAirKermaRate
+
+        with open(directory+'/sourcespec.yaml','r') as stream:
+            sourcespec = yaml.load(stream)
+
+        source = sourcespec[rp.BrachyTreatmentType]
+
+        if Tag(0x300a, 0x21a) in rp.SourceSequence[0].keys():
+            self.L = rp.SourceSequence[0].ActiveSourceLength/10
+        elif 'length' in source.keys():
+            self.L = source['length']
+        else:
+            raise ValueError('Source length data missing!')
+
+        try:
+            self.Delta = source['delta']
+
+            Fi = np.loadtxt(directory+'/'+source['anisotropy']['filename'], delimiter=',')
+            self.Fi = interp2d(Fi[0,:][1:],Fi[:,0][1:],Fi[1:,1:])
+
+            if 'filename' in source['radial'].keys():
+                g = np.loadtxt(directory+'/'+source['radial']['filename'], delimiter=',')
+                self.gi = interp1d(g[:,0],g[:,1])
+            else:
+                self.coeff = source['radial']['coeff']
+        except KeyError as e:
+            raise ValueError('Source data not specified correctly!')
+
+        self.G0 = 2*np.arctan((self.L/2)/r0)/(self.L*r0*np.sin(theta0))
+
+    def F(self, r, theta):
+        """Anisotropy function.
+
+        Args:
+            r: distance (cm).
+            theta: angle (radians).
+        """
+        if r > 15:
+            return self.Fi(15,theta)
+        else:
+            return self.Fi(r,theta)
+    def g(self, r):
+        """Radial dose function.
+
+        Args:
+            r: distance (cm).
+        """
+        if hasattr(self,'gi'):
+            return self.gi(r)
+        else:
+            if r < 0.15:
+                return self.g(0.15)
+            elif r > 15:
+                return self.g(12)*np.exp((r-12)/(15-12)*(np.log(self.g(15))-np.log(self.g(12))))
+            nth = len(self.coeff)
+            out = 0
+            for n in range(nth):
+                out += self.coeff[n]*r**n
+            return out
 
 
 class Dwell(object):
@@ -57,7 +140,7 @@ class Dwell(object):
             coords: list containing x, y, and z coordinates of dwell point.
             t: dwell time.
             L: source length.
-            app: applicator object dwell point is a part of.
+            app: applicator ROI object dwell point is a part of.
         """
 
         self.x, self.y, self.z = coords
@@ -66,7 +149,12 @@ class Dwell(object):
         self.get_source_ends(app, L)
 
     def get_source_ends(self, app, L):
-        """Calculates coordinates of the ends of the source"""
+        """Calculates coordinates of the ends of the source.
+
+        Args:
+            app: applicator ROI object dwell point is a part of.
+            L: source length (cm).
+        """
         closestd = [+np.inf,+np.inf]
         closest = [0,0]
         for pt in app.coords:
@@ -86,7 +174,7 @@ class Dwell(object):
 
 
 class DosePoint(object):
-    """Point at which dose is calcluated
+    """Point at which dose is calcluated.
 
     Attributes:
         x, y, z: x, y and z coordinates of point (cm).
@@ -96,7 +184,7 @@ class DosePoint(object):
         dose: calculated dose (cGy).
         tpsdose: TPS dose obtained from DICOM file (cGy).
     """
-    def __init__(self, coords, rp, rs, name='', ref='', directory='.'):
+    def __init__(self, coords, source, plan, name='', ref='', directory='.'):
         """
         Args:
             coords: list of dose point coordinates
@@ -110,8 +198,10 @@ class DosePoint(object):
         self.coords = np.array(coords)
         self.name = name
         self.ref = ref
-        self.get_tpsdose(rp)
-        self.calc_dose(rp, rs, directory)
+        self.source = source
+        if self.ref != '':
+            self.get_tpsdose(plan.rp)
+        self.calc_dose(source, plan)
 
     def __repr__(self):
         return self.name
@@ -144,71 +234,73 @@ class DosePoint(object):
 
         self.tpsdose = tpsdose * 100
 
-    def calc_dose(self, rp, rs, directory):
+    def calc_dose(self, source, plan):
         """Calculate dose for this point
 
         Args:
+            source: Source object.
+            plan: Plan object.
+        """
+        dose = 0
+        for d in plan.dwells:
+            if d.t > 0:
+                r = euclidean(self.coords,d.middle)
+                r1 = euclidean(self.coords,d.ends[1])
+                r2 = euclidean(self.coords,d.ends[0])
+                theta = np.arccos(np.dot((d.ends[0]-d.ends[1])/source.L,(self.coords - d.middle)/r))
+                theta1 = np.arccos(np.dot((d.ends[0]-d.ends[1])/source.L,(self.coords - d.ends[1])/r1))
+                theta2 = np.arccos(np.dot((d.ends[0]-d.ends[1])/source.L,(self.coords - d.ends[0])/r2))
+                if (theta < 0.003) or ((np.pi-theta) < 0.003):
+                    G = 1/(r**2 - source.L**2/4)
+                else:
+                    B = np.abs(theta2 - theta1)
+                    G = B/(source.L*r*np.sin(theta))
+
+                dose += source.Sk * source.Delta * (G/source.G0) * source.g(r) * source.F(r,np.degrees(theta))[0] * d.t / 3600
+
+        self.dose = dose
+
+
+class Plan(object):
+    """Plan parameters.
+
+    Attributes:
+        rp: pydicom object of RP file.
+        ROIs: list of ROI objects in plan.
+        dwells: list of Dwell objects in plan.
+    """
+    def __init__(self, source, rp, rs):
+        """
+        Args:
+            source: Source object.
             rp: pydicom object of RP file.
             rs: pydicom object of RS file.
-            directory: directory containing source data.
         """
-        r0 = 1
-        theta0 = np.pi/2
-        Sk = rp.SourceSequence[0].ReferenceAirKermaRate
+        self.rp = rp
+        self.get_ROIs(rs)
+        self.get_dwells(source, rp)
+    def get_ROIs(self, rs):
+        """Get all structures in plan.
 
-        with open(directory+'/sourcespec.yaml','r') as stream:
-            sourcespec = yaml.load(stream)
-
-        source = sourcespec[rp.BrachyTreatmentType]
-
-        if Tag(0x300a, 0x21a) in rp.SourceSequence[0].keys():
-            L = rp.SourceSequence[0].ActiveSourceLength/10
-        elif 'length' in source.keys():
-            L = source['length']
-        else:
-            raise ValueError('Source length data missing!')
-
-        try:
-            Delta = source['delta']
-
-            Fi = np.loadtxt(directory+'/'+source['anisotropy']['filename'], delimiter=',')
-            Fi = interp2d(Fi[0,:][1:],Fi[:,0][1:],Fi[1:,1:])
-            def F(r,theta):
-                if r > 15:
-                    return Fi(15,theta)
-                else:
-                    return Fi(r,theta)
-
-            if 'filename' in source['radial'].keys():
-                g = np.loadtxt(directory+'/'+source['radial']['filename'], delimiter=',')
-                g = interp1d(g[:,0],g[:,1])
-            else:
-                coeff = source['radial']['coeff']
-                def g(r):
-                    if r < 0.15:
-                        return g(0.15)
-                    elif r > 15:
-                        return g(12)*np.exp((r-12)/(15-12)*(np.log(g(15))-np.log(g(12))))
-                    nth = len(coeff)
-                    out = 0
-                    for n in range(nth):
-                        out += coeff[n]*r**n
-                    return out
-        except KeyError as e:
-            raise ValueError('Source data not specified correctly!')
-
-        G0 = 2*np.arctan((L/2)/r0)/(L*r0*np.sin(theta0))
-
-        ROIs = []
+        Args:
+            rs: pydicom object of RS file.
+        """
+        self.ROIs = []
         for struct in rs.StructureSetROISequence:
-            ROIs.append(ROI(struct.ROINumber, struct.ROIName, rs))
+            self.ROIs.append(ROI(struct.ROINumber, struct.ROIName, rs))
+    def get_dwells(self, source, rp):
+        """Get all dwell points in plan.
 
+        Args:
+            source: Source object.
+            rp: pydicom object of RP file.
+        """
         time = 0
-        dwells = []
+        self.dwells = []
         for cath in rp[0x300a, 0x230][0][0x300a, 0x280]:
             dwell_pts = cath[0x300a, 0x2d0]
 
-            for roi in ROIs:
+            for roi in self.ROIs:
                 if cath.SourceApplicatorID == roi.name:
                     app = roi
 
@@ -216,26 +308,7 @@ class DosePoint(object):
                 x, z, y = d[0x300a, 0x2d4].value
                 t = d[0x300a, 0x2d6].value - time
                 time = d[0x300a, 0x2d6].value
-                dwells.append(Dwell([x/10, y/10, z/10], t, L, app))
-
-        dose = 0
-        for d in dwells:
-            if d.t > 0:
-                r = euclidean(self.coords,d.middle)
-                r1 = euclidean(self.coords,d.ends[1])
-                r2 = euclidean(self.coords,d.ends[0])
-                theta = np.arccos(np.dot((d.ends[0]-d.ends[1])/L,(self.coords - d.middle)/r))
-                theta1 = np.arccos(np.dot((d.ends[0]-d.ends[1])/L,(self.coords - d.ends[1])/r1))
-                theta2 = np.arccos(np.dot((d.ends[0]-d.ends[1])/L,(self.coords - d.ends[0])/r2))
-                if (theta < 0.003) or ((np.pi-theta) < 0.003):
-                    G = 1/(r**2 - L**2/4)
-                else:
-                    B = np.abs(theta2 - theta1)
-                    G = B/(L*r*np.sin(theta))
-
-                dose += Sk * Delta * (G/G0) * g(r) * F(r,np.degrees(theta))[0] * d.t / 3600
-
-        self.dose = dose
+                self.dwells.append(Dwell([x/10, y/10, z/10], t, source.L, app))
 
 
 class ROI(object):
@@ -259,7 +332,10 @@ class ROI(object):
         self.get_coords(rs)
 
     def get_coords(self, rs):
-        """Get co-ordinates for this structure from the RS file."""
+        """Get co-ordinates for this structure from the RS file.
+
+        Args:
+            rs: pydicom object of RS file."""
         for roi in rs.ROIContourSequence:
             if roi[0x3006,0x84].value == self.number and Tag(0x3006, 0x40) in roi.keys():
                 for sli in roi.ContourSequence:
